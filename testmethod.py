27f0592d8d0022d4780f1bcd84719767755bdf6b
potter444 <469214976@qq.com>
2021-08-24 21:45:37 +0800

    封装游戏client的方法

--- testmethod.py
+++ testmethod.py
@@ -7,18 +7,42 @@ import struct
 from testproto.proto_py.testProtocolCode_pb2 import *
 from testproto.proto_py.Test_pb2 import *
 
+"""
+获取游戏的协议，参数，协议号，实例方法，返回一个字典
+格式：protoData={"C2SLogin":{"cmd":1001, "args": {"account": "", "connectKey": ""}, "method": "login"},
+                    "C2SHeart": {"cmd": 1002, "args": {"account": ""}, "method": "heart"}
+                    }
+"""
+protoData = {}
 
 
-def get_testc2s_protobuf():
-    d = {}
-    for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass):
-        if name[:3] == "C2S":
-            l = []
-            for i in eval(name).DESCRIPTOR.fields:
-                l.append(i.name)
-            d.setdefault(name, l)
-    print("客户端发送的协议及参数：", d)
-    return d
+def get_testgame_protobuf():
+    global protoData
+    for name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj):
+            if name[:3] == "C2S":
+                l = {}
+                for i in eval(name).DESCRIPTOR.fields:
+                    l.setdefault(i.name, "")
+                if name not in protoData:
+                    protoData.setdefault(name, {"args": l})
+                else:
+                    protoData[name]["args"] = l
+
+        elif inspect.isfunction(obj):
+            fullargs = inspect.getfullargspec(obj)
+
+            if len(fullargs.args) == 2 and fullargs.args == ['cmd', 'method']:
+                if fullargs.defaults[1] not in protoData:
+                    protoData.setdefault(fullargs.defaults[1], {})
+                    protoData[fullargs.defaults[1]]["cmd"] = fullargs.defaults[0]
+                    protoData[fullargs.defaults[1]]["method"] = name
+                else:
+                    protoData[fullargs.defaults[1]]["cmd"] = inspect.getfullargspec(obj).defaults[0]
+                    protoData[fullargs.defaults[1]]["method"] = name
+
+    print("客户端发送的协议及参数：", protoData)
+    return protoData
 
 
 def get_tests2c_protobuf():
@@ -32,42 +56,64 @@ def get_tests2c_protobuf():
     print("服务器返回的协议及参数：", d)
     return d
 
+# 数据格式解析
+def get_int_value(data: str):
+    return int(data)
 
-def send_pack(sock, cmd, data):
+def get_str_value(data: str):
+    return data
+
+
+# 按照格式，封包，发送
+def testgame_pack(cmd, data):
     length = len(data)
     fmt = ">iih%ss" % length
     d = struct.pack(fmt, (length + 6), cmd, 0, data)
     print("发送的协议号：", cmd)
-    sock.send(d)
+    return d
 
 
-def login(sock):
-    login = C2SLogin()
-    login.account = "po001"
-    login.connectKey = "fxltsbl"
-    print(login)
-    data = login.SerializeToString()
-    print(data)
-    send_pack(sock, 1001, data)
+def login(cmd=1001, method="C2SLogin"):  # 登陆协议实例化
+    l = C2SLogin()
+    data = protoData.get(method)
+    l.account = get_str_value(data.get("args").get("account"))
+    l.connectKey = get_str_value(data.get("args").get("connectKey"))
+    data = l.SerializeToString()
+    return data
 
 
-def heart(sock):
+def heart(cmd=1002, method="C2SHeart"): # 心跳协议实例化
+    data = protoData.get(method)
     h = C2SHeart()
-    h.account = "po001"
+    h.account = get_str_value(data.get("args").get("account"))
     data = h.SerializeToString()
-    send_pack(sock, 1002, data)
+    return data
 
 
-def socker_client():
+# 协议发送的方法，传入数据，sock
+def testgame_send(sock, protoID):
+    proto = protoData.get(protoID).get("method")
+    proto = eval(proto)()
+    cmd = protoData.get(protoID).get("cmd")
+    data = testgame_pack(cmd, proto)
+    sock.send(data)
+
+
+# 协议接收方法，接收回包，返回结果
+def testgame_recv(sock, cmd):
+    pass
+
+
+def socket_client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("192.168.10.143", 21566))
     while True:
 
         num = int(input("输入数字："))
         if num == 1:
-            login(sock)
+            testgame_send(sock, "C2SHeart")
         elif num == 2:
-            heart(sock)
+            testgame_send(sock, "C2SLogin")
         elif num == 4:
             continue
         else:
@@ -76,4 +122,7 @@ def socker_client():
 
 
 if __name__ == "__main__":
-    socker_client()
+
+    # get_all_proto()
+    get_testgame_protobuf()
+    socket_client()
--- toolGui.py
+++ toolGui.py
@@ -13,6 +13,7 @@ class ToolsWindows(Ui_MainWindow, QtWidgets.QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.game = ""
+        self.argvlist = []  # 协议参数的列表
         self.loginframe.setVisible(True)
         self.protoframe.setVisible(False)
         self.protofileEdit.setVisible(False)
@@ -22,10 +23,11 @@ class ToolsWindows(Ui_MainWindow, QtWidgets.QMainWindow):
         self.gameselect.addItems(gamelist)
         self.gameselect.activated[str].connect(self.get_server)
 
-        self.protofileBox.activated[str].connect(self.selectprotofile)
+        self.protofileBox.activated[str].connect(self.select_proto_file)
 
         self.loginButton.clicked.connect(self.login_game)
         self.quitbutton.clicked.connect(self.relogin)
+        self.pushButton.clicked.connect(self.send_proto)
 
     def get_server(self, game):  # 根据游戏选择服务器地址
         self.severselect.clear()
@@ -52,8 +54,8 @@ class ToolsWindows(Ui_MainWindow, QtWidgets.QMainWindow):
         self.loginframe.setVisible(True)
         self.protoframe.setVisible(False)
 
-
-    def selectprotofile(self, filename):
+    # 选择协议文件，
+    def select_proto_file(self, filename):
         # self.protofileBox.clear()
         with open("%s/%s" % (config.gameDict.get(self.game).get("filepath"), filename), "r", encoding="utf-8") as f:
             data = f.read()
@@ -62,14 +64,19 @@ class ToolsWindows(Ui_MainWindow, QtWidgets.QMainWindow):
 
     # 选择协议时，调用的方法。自动生成参数列表
     def select_proto(self, proto):
-        argvlist = self.c2sproto.get(proto)
-        if len(argvlist) == 0:
-            return
-        print(argvlist)
+        self.argvlist.clear()
+        self.argvlist = self.c2sproto.get(proto)
+
+        # 先清空form layout
         for i in range(self.argsformLayout.count()):
             self.argsformLayout.itemAt(i).widget().deleteLater()
 
-        for i in argvlist:
+        if len(self.argvlist) == 0:
+            return
+        print(self.argvlist)
+
+        # 遍历参数列表，生成QLineEdit
+        for i in self.argvlist:
             value_1 = QtWidgets.QLineEdit()
 
             self.argsformLayout.addRow("%s:" % i, value_1)
@@ -77,6 +84,16 @@ class ToolsWindows(Ui_MainWindow, QtWidgets.QMainWindow):
 
     # 发送协议，获取填写的proto,argv;并将结果输出到文本框
     def send_proto(self):
+        argv_value = []
+        protoId = self.proto_name.currentText()
+        for i in range(self.argsformLayout.count()):
+            if self.argsformLayout.itemAt(i).widget().inherits('QLineEdit'):
+                argv_value.append(self.argsformLayout.itemAt(i).widget().text())
+
+        print("发送的协议和参数：", protoId, argv_value)
+
+    # 把回包解析，输出在界面上
+    def show_result(self, data):
         pass
 
 
